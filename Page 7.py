003e6f70bc4bc2a718d544abf169fb6ed8d53906
alejandrovaras <70179469+alejandrovaras@users.noreply.github.com>
2020-09-07 08:04:09 -0400

    Add files via upload

---
+++ Page 7.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+Loaction = "all_040_in_01.P1.csv"
+
+
+# In[3]:
+
+
+df = pd.read_csv(Loacation, header=None)
+
+
+# In[4]:
+
+
+df = pd.read_csv(Location, header=None)
+
+
+# In[ ]:
+
+
+
+
